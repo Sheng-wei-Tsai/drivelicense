b2d3aedb9648e30515a5c1712b382a48bca2a4fe
蔡昇偉 <henry88002605@gmail.com>
2019-11-16 18:51:16 +0800

    taiwan_law

---
+++ drivelicense.py
@@ -0,0 +1,9 @@
+country = input('Where are you from: ')
+age = input('How old are you: ')
+age = int(age)
+if country == 'taiwan':
+	if age >= 18:
+		print('you can get drivelicense')
+	else:
+		print('you cannot get drivelicense')
+
