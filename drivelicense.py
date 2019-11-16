2bdb15911829db3cf9f171d208d762008476061d
蔡昇偉 <henry88002605@gmail.com>
2019-11-16 19:00:06 +0800

    add american part

--- drivelicense.py
+++ drivelicense.py
@@ -3,7 +3,14 @@ age = input('How old are you: ')
 age = int(age)
 if country == 'taiwan':
 	if age >= 18:
-		print('you can get drivelicense')
+		print('you can get drivelicense')  
 	else:
 		print('you cannot get drivelicense')
+elif country == 'american':
+	if age >= 16:
+		print('you can get drivelicense')
+	else: 
+		print('you cannot get drivelicense')
+
+		
 
